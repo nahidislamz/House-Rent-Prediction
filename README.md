4fae5581523aee5f9118436ddf66d9461d316a7e
Nahid Islam <nahidislamz@outlook.com>
2021-10-22 12:23:33 +0600

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # House Rent Prediction Prjoect
+## A data science project that predict rent of houses in an area
--- home/views.py
+++ home/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from . import util
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    
+    if request.method == 'POST':
+
+        total_sqft = float(request.POST.get('area_size'))
+        location = request.POST.get('location')
+        bhk = int(request.POST.get('bhk'))
+        bath = int(request.POST.get('bath'))
+
+        util.load_saved_artifacts()
+        print(total_sqft," location: ",location," BHK: ",bhk," BATH: ",bath)
+        estimated_price = util.get_estimated_price(location,total_sqft,bhk,bath)
+        print("estimated_price = ",estimated_price)
+        context ={
+            'locations': util.get_location_names(),
+            'estimated_price': estimated_price,
+            'total_sqft':total_sqft,
+            'bath':bath,
+            'location':location,
+            'bhk':bhk
+            }
+        
+    return render(request,'index.html',context)
