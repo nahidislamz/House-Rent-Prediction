ff17e27365c5a178af6f6ccdca17683d7a3aa3a7
Nahid Islam <nahidislamz@outlook.com>
2021-10-22 15:35:38 +0600

    few bug fixed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class History(models.Model):
+
+    location = models.CharField(max_length=500, null=True)
+    area = models.FloatField()
+    bedrooms = models.IntegerField()
+    bathroom = models.IntegerField()
+    estimated_price = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.location
--- home/views.py
+++ home/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
 from . import util
+from .models import History
 # Create your views here.
 def home(request):
     
+   
+    util.load_saved_artifacts()
+    locations = util.get_location_names()
+    estimated_price = ""
+    total_sqft = 0.00
+    location = ""
+    bhk = 0
+    bath = 0
+    history = History.objects.all()
     if request.method == 'POST':
 
         total_sqft = float(request.POST.get('area_size'))
         location = request.POST.get('location')
         bhk = int(request.POST.get('bhk'))
         bath = int(request.POST.get('bath'))
-
-        util.load_saved_artifacts()
-        print(total_sqft," location: ",location," BHK: ",bhk," BATH: ",bath)
         estimated_price = util.get_estimated_price(location,total_sqft,bhk,bath)
-        print("estimated_price = ",estimated_price)
-        context ={
-            'locations': util.get_location_names(),
-            'estimated_price': estimated_price,
-            'total_sqft':total_sqft,
-            'bath':bath,
-            'location':location,
-            'bhk':bhk
-            }
+                
+    context ={
+        'locations': locations,
+        'estimated_price': estimated_price,
+        'total_sqft':total_sqft,
+        'bath':bath,
+        'location':location,
+        'bhk':bhk,
+        'history':history,
+        }
         
     return render(request,'index.html',context)
