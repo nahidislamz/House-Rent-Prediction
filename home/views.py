1f141751c435da7d6907f658c0fd5535ee283acb
Nahid Islam <nahidislamz@outlook.com>
2021-10-22 16:08:24 +0600

    history added

--- home/views.py
+++ home/views.py
@@ -7,12 +7,16 @@ def home(request):
    
     util.load_saved_artifacts()
     locations = util.get_location_names()
+    history = History.objects.all().order_by('-id')[0:3]
     estimated_price = ""
     total_sqft = 0.00
     location = ""
     bhk = 0
     bath = 0
-    history = History.objects.all()
+
+    save_history = History()
+
+
     if request.method == 'POST':
 
         total_sqft = float(request.POST.get('area_size'))
@@ -20,7 +24,14 @@ def home(request):
         bhk = int(request.POST.get('bhk'))
         bath = int(request.POST.get('bath'))
         estimated_price = util.get_estimated_price(location,total_sqft,bhk,bath)
-                
+        
+        save_history.area = float(request.POST.get('area_size'))
+        save_history.location = request.POST.get('location')
+        save_history.bedrooms = int(request.POST.get('bhk'))
+        save_history.bathroom = int(request.POST.get('bath'))
+        save_history.estimated_price = util.get_estimated_price(location,total_sqft,bhk,bath)
+        save_history.save()   
+
     context ={
         'locations': locations,
         'estimated_price': estimated_price,
@@ -32,3 +43,11 @@ def home(request):
         }
         
     return render(request,'index.html',context)
+
+
+def historyView(request):
+    history = History.objects.all().order_by('-id')
+    context={
+        'history':history
+    }
+    return render(request,'history.html',context)
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
-from home.views import home
+from home.views import home,historyView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
+    path('',home , name='home'),
+    path('history/',historyView,name='history')
 ]
